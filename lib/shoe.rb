d0cdcfa9dabce452bf82033b576252f9a85c43a5
Robin Kim <rkim1214@gmail.com>
2018-11-21 20:00:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,6 @@
 class Shoe
-  attr_accessor :color, :size
+  attr_accessor :color, :size, :material
+  
   def initialize(brand)
     @brand = brand
   end 
@@ -7,6 +8,4 @@ class Shoe
   def brand 
     @brand
   end 
-  
-
 end 
